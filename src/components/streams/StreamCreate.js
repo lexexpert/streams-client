9d5e68e31ad126e6571a042a8f015f8d717404ef
Ivan <ivanreshetar@gmail.com>
2019-05-25 21:19:45 -0500

    Redux form and validation created

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,55 @@
 import React from 'react';
+import {Field, reduxForm} from "redux-form";
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>
+class StreamCreate extends React.Component {
+  renderError = ({error, touched}) => {
+    if(touched && error) {
+      return (
+        <div className="ui error message">
+          {error}
+        </div>
+      );
+    }
+  };
+
+  renderInput = ({input, label, meta}) => {
+    const fieldClassName = `field ${meta.error && meta.touched ? 'error' : ''}`;
+    return (
+      <div className={fieldClassName}>
+        <label>{label}</label>
+        <input {...input} autoComplete="off"/>
+        {this.renderError(meta)}
+      </div>
+      )
+  };
+
+  onSubmit(formValues) {
+
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
+        <Field name="title" component={this.renderInput} label="Enter title" />
+        <Field name="description" component={this.renderInput} label="Enter description" />
+        <button className="ui button primary">Submit</button>
+      </form>
+    )
+  }
+}
+
+const validate = (formValues) => {
+  const errors = {};
+  if(!formValues.title) {
+    errors.title = 'You must enter a title';
+  }
+  if(!formValues.description) {
+    errors.description = 'You must enter a description';
+  }
+  return errors;
 };
 
-export default StreamCreate;
+export default reduxForm({
+  form: 'streamCreate',
+  validate
+})(StreamCreate);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from "redux";
+import {createStore, applyMiddleware, compose} from "redux";
 
 import './index.css';
 import App from './App';
 import reducers from './reducers';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
 ReactDOM.render(
   <Provider store={store}>
